148cac77b65a128f2f172e5ca6c5d204823d2a30
Youssef Gamil <yoga1290@gmail.com>
2015-12-05 17:15:23 +0200

    FBID as separate field

--- src/main/java/videoquotes/corn/FacebookPostAnalytics.java
+++ src/main/java/videoquotes/corn/FacebookPostAnalytics.java
@@ -5,45 +5,15 @@
  */
 package videoquotes.corn;
 
-import com.google.appengine.api.datastore.DatastoreServiceFactory;
-import com.google.appengine.api.datastore.Entity;
-import com.google.appengine.api.datastore.FetchOptions;
-import com.google.appengine.api.datastore.KeyFactory;
-import com.google.appengine.api.datastore.PreparedQuery;
 import java.util.Collection;
 import java.util.Date;
 import java.util.Iterator;
-import java.util.List;
-import java.util.Queue;
-import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import videoquotes.errorMessages.FacebookSharingFailed;
-import videoquotes.repository.Channel;
-import videoquotes.repository.ChannelRepository;
-import videoquotes.repository.FBUser;
-import videoquotes.repository.FBUserRepository;
 import videoquotes.repository.FacebookPost;
-import videoquotes.repository.FacebookPostQueue;
-import videoquotes.repository.FacebookPostQueueRepository;
 import videoquotes.repository.FacebookPostRepository;
-import videoquotes.repository.Person;
-import videoquotes.repository.PersonRepository;
-import videoquotes.repository.Quote;
-import videoquotes.repository.QuoteAnalytics;
-import videoquotes.repository.QuoteAnalyticsRepository;
-import videoquotes.repository.QuoteOwner;
-import videoquotes.repository.QuoteOwnerRepository;
-import videoquotes.repository.QuoteRepository;
-import videoquotes.repository.Video;
-import videoquotes.repository.VideoRepository;
 import videoquotes.util.FacebookUtil;
-import videoquotes.util.QuoteSvc;
-import videoquotes.util.YoutubeUtil;
 
 /**
  *
@@ -55,15 +25,15 @@ public class FacebookPostAnalytics {
     @Autowired
     private FacebookPostRepository posts;
 
-    @RequestMapping(value = "/corn/updatePostAnalytics")
+    @RequestMapping(value = "/cron/updatePostAnalytics")
     private void updateQuoteAnalytics(){
 	Collection<FacebookPost> queuedQuotes= posts.findByNeedSync(0, 1);
 	Iterator<FacebookPost> queuedQuotesIt= queuedQuotes.iterator();
 	while(queuedQuotesIt.hasNext())
 	{
 	    FacebookPost post=queuedQuotesIt.next();	    
-	    post.setLikes(new Long( FacebookUtil.getPostLikesCount(post.getQuoteId())));
-	    post.setShares(new Long( FacebookUtil.getPostShareCount(post.getQuoteId())));
+	    post.setLikes(new Long( FacebookUtil.getPostLikesCount(post.getFbid())));
+	    post.setShares(new Long( FacebookUtil.getPostShareCount(post.getFbid())));
 	    post.setLastSync(new Date().getTime());
 	    posts.update(post);
 	}
