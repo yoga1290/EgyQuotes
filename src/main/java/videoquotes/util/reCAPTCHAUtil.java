3684638df0a414934a4bc60d17ce0f6069ddafb8
Youssef Gamil <yoga1290@gmail.com>
2016-01-22 21:28:13 +0200

    refactoring: Utils classes

---
+++ src/main/java/videoquotes/util/reCAPTCHAUtil.java
@@ -0,0 +1,25 @@
+package videoquotes.util;
+
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import videoquotes.Credential;
+
+/**
+ *
+ * @author yoga1290
+ */
+@Component
+public class reCAPTCHAUtil {
+    
+    @Autowired
+    private URLUtil url;
+    
+    // https://developers.google.com/recaptcha/docs/verify
+    public String verify(String captcha) throws Exception
+    {
+	return 
+		url.sendPost("https://www.google.com/recaptcha/api/siteverify", "secret="+Credential.reCAPTCHA.SECRET+"&response="+captcha);
+    }
+    
+}
