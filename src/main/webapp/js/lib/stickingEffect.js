dc39fe647c459cf2dcd01f83c2cda726396f2479
Youssef Gamil <yoga1290@gmail.com>
2016-02-05 15:27:08 +0200

    stickingEffect fix

--- src/main/webapp/js/lib/stickingEffect.js
+++ src/main/webapp/js/lib/stickingEffect.js
@@ -3,14 +3,21 @@ var stickingEffect=(function(){
 
     var initPosition=[];
 
-    function scan(){
-	$('.stickable').not('[initposition]').each(function(i,o){
-	    $(o).attr('initposition',$(o).position().top);
-	    $(document).scroll(function(){
-		if( $('body').scrollTop()>=$(o).attr('initposition'))
+    $(function(){
+	$(document).scroll(function(){
+	    $('.stickable').each(function(i,o){
+		if( $('body').scrollTop()>=parseInt($(o).attr('initposition')))
 		    $(o).addClass('stickOnTop');
 		else
 		    $(o).removeClass('stickOnTop');
+	    });
+	});
+    });
+    
+    function scan(){
+	$(function(){
+	    $('.stickable').not('[initposition]').each(function(i,o){
+		$(o).attr('initposition',$(o).position().top);
 	    });
 	});
     }
