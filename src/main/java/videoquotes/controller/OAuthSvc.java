f63e80a1bc407a53d8c5c1814409a118e5bc89f3
Youssef Gamil <yoga1290@gmail.com>
2016-05-03 15:54:51 +0200

    VideoSvc: Cache some request data

--- src/main/java/videoquotes/controller/OAuthSvc.java
+++ src/main/java/videoquotes/controller/OAuthSvc.java
@@ -6,59 +6,17 @@ import videoquotes.repository.*;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
 import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
 import java.net.HttpURLConnection;
-import java.net.Socket;
 import java.net.URL;
-import java.util.Properties;
-
-import javax.mail.Message;
-import javax.mail.MessagingException;
-import javax.mail.Session;
-import javax.mail.Transport;
-import javax.mail.internet.AddressException;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
 import javax.servlet.http.HttpServletResponse;
-
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.List;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.fasterxml.jackson.databind.util.JSONPObject;
-import com.google.gson.JsonObject;
-
-import videoquotes.repository.User;
 import videoquotes.repository.UserRepository;
 import videoquotes.util.*;
 
@@ -76,7 +34,7 @@ public class OAuthSvc
 
 
 
-	//	https://www.facebook.com/dialog/oauth?client_id=1048399691842414&redirect_uri=https://videoquotes.appspot.com/OAuth/facebook/&scope=email,manage_pages&state=/index
+	//	https://www.facebook.com/dialog/oauth?client_id=&redirect_uri=&state=/index
 	@RequestMapping(value=Credential.OAuth.facebook.REDIRECT_URI, method=RequestMethod.GET)
 	public @ResponseBody void facebook(@RequestParam String code,@RequestParam String state,HttpServletResponse response) throws IOException//@RequestParam String videoId)
 	{
@@ -86,10 +44,10 @@ public class OAuthSvc
 	    if(!users.exists(facebookId))
 		    users.save(new FBUser(facebookId));
 	    
-	    response.sendRedirect(Credential.BASE_URL+state+".html?access_token="+access_token);
+	    response.sendRedirect(Credential.BASE_URL+state+".html#access_token="+access_token);
 	}
 
-	// https://accounts.google.com/o/oauth2/auth?redirect_uri=https://beta.videoquotes.appspot.com/OAuth/google/&response_type=code&client_id=730812089934-7dprqgn1ggh6gkvhp7n63d1qktbmo89b.apps.googleusercontent.com&approval_prompt=auto&scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.email+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.profile&access_type=offline
+	// https://accounts.google.com/o/oauth2/auth?redirect_uri=/&response_type=code&client_id=&approval_prompt=auto&scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.email+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.profile&access_type=offline
 	@RequestMapping(value=Credential.OAuth.google.REDIRECT_URI, method=RequestMethod.GET)
 	public @ResponseBody String google(@RequestParam String code)
 	{
@@ -149,90 +107,4 @@ public class OAuthSvc
 
 		return res;
 	}
-
-	
-	
-//	@RequestMapping(value="/users", method=RequestMethod.GET)
-//	public @ResponseBody Collection<FBUser> getUserList(@RequestParam int offset,@RequestParam int limit){
-//		return (List<FBUser>)(users.findAll());
-//	}
-//	
-//	@RequestMapping(value="/user/{id}", method=RequestMethod.GET)
-//	public @ResponseBody User getUserList(
-//								@PathVariable long id,
-//								@RequestParam String access_token,
-//								HttpServletResponse response)
-//								throws Exception
-//	{
-//		try{
-//			User user=users.findByFacebookAccessToken(access_token);
-//			if(!user.getId().equals(id))
-//				return null;
-//		}catch(Exception ew){
-//			response.sendError(403, "FORBIDDEN!! UserId must match with the access token owner" );
-//			return null;
-//		}
-//		
-//		
-//		return users.findOne(id);
-//	}
-//	
-//	
-//	
-////	https://www.facebook.com/dialog/oauth?client_id=1048399691842414&redirect_uri=https://videoquotes.appspot.com/OAuth/facebook/&scope=email,manage_pages&state=SOME_ARBITRARY_BUT_UNIQUE_STRING
-//		@RequestMapping(value="/OAuth/facebook/", method=RequestMethod.GET)
-//		public @ResponseBody void insert(@RequestParam String code,@RequestParam String state,HttpServletResponse response) throws IOException//@RequestParam String videoId)
-//		{
-//			String access_token=readText("https://graph.facebook.com/oauth/access_token?client_id=1048399691842414&redirect_uri=https://videoquotes.appspot.com/OAuth/facebook/&client_secret=49dddc949088ee58f23bc174c9baa20b&code="+code);
-//			access_token=access_token.substring(access_token.indexOf('=')+1);
-//			String facebookId=readText("https://graph.facebook.com/me?access_token="+access_token);
-//			facebookId=facebookId.substring(facebookId.indexOf("\"id\":\"")+6,facebookId.indexOf("\","));
-//			
-//			//TODO: 
-////			if(!users.exists(facebookId))
-////				users.save(new User(facebookId));
-//			//TODO: redirect
-//			response.sendRedirect("https://.appspot.com/"+state+".html?access_token="+access_token);
-////			return access_token;
-//		}
-//		
-//		
-//		
-//		
-//		
-//		@RequestMapping(value="/User/DataTable",produces="application/json", method=RequestMethod.POST)
-//		public @ResponseBody dataTableResponse datatable(
-//				HttpServletResponse									response,
-//				@RequestParam(value="access_token")					String	access_token,
-//				
-//				@RequestParam(value="draw") 							int 		draw,
-//				@RequestParam(value="start") 						int 		start,
-//				@RequestParam(value="length") 						int 		length,
-//				@RequestParam(value="search[value]") 				String 	search,
-//				//custom; see 
-//				@RequestParam(value="order")			 				String  	orderDir,
-//				@RequestParam(value="colName") 						String  	colName
-//			) throws Exception
-//			{
-//			
-//			User user=null;
-//			try{
-//				user=users.findByFacebookAccessToken(access_token);
-//				if(!user.getFacebook().equals(Credential.ADMIN_USER_ID))
-//					return null;
-//			}catch(Exception ew){
-//				response.sendError(404, ew.getLocalizedMessage() );
-//				return null;
-//			}
-//			
-//			
-//				try{
-//					Collection data=users.find(search,colName,orderDir.equals("asc"),start, length);
-//					int count=users.count();
-//					return new dataTableResponse(draw,count,count,data);
-//				}catch(Exception e){
-//					response.sendError(500, e.getLocalizedMessage()+"");
-//					return null;
-//				}
-//			}
 }
--- src/main/webapp/js/svc/VideoSvc.js
+++ src/main/webapp/js/svc/VideoSvc.js
@@ -34,14 +34,68 @@ app.service('VideoSvc',['$http', '$q', function($http, $q){
 	};
 	
 	var YTKey='AIzaSyBNWNnIBvHyBaREu7ikXf7jqW_865D4CD0';
+	var _getChannelData = {};
 	this.getChannelData=function(channelId){
+	    if(_getChannelData[channelId] !== undefined)
+		return _getChannelData[channelId];
+	    
+	    var asyncTask = $q.defer();
 	    var chain=
-		    $http.get('https://www.googleapis.com/youtube/v3/channels?part=snippet&key='+YTKey+'&id='+channelId);
-	    return chain;
+		    $http.get('https://www.googleapis.com/youtube/v3/channels?part=snippet&key='+YTKey+'&id='+channelId)
+		    .success(function(response){
+			asyncTask.resolve(response);
+		    })
+		    .error(function(reason){
+			_getChannelData[channelId] = undefined;
+			asyncTask.reject(reason);
+		    });
+	    asyncTask.promise.success=function(cb){
+		asyncTask.promise.then(cb,function(){});
+		return asyncTask.promise;
+	    };
+	    asyncTask.promise.error=function(cb){
+		asyncTask.promise.then(function(){},cb);
+		return asyncTask.promise;
+	    };
+	    asyncTask.promise.finally=function(cb){
+		asyncTask.promise.then(cb,cb);
+		return asyncTask.promise;
+	    };
+	    
+	    _getChannelData[channelId] = asyncTask.promise;
+	    
+	    return asyncTask.promise;
 	};
+	
+	var _getChannelId = {};
 	this.getChannelId=function(videoId){
+	    if(_getChannelId[videoId] !== undefined)
+		return _getChannelId[videoId];
+	    var asyncTask = $q.defer();
+	    
 	    var chain=
-		    $http.get('https://www.googleapis.com/youtube/v3/videos?part=snippet&key='+YTKey+'&id='+videoId);
-	    return chain;
+		    $http.get('https://www.googleapis.com/youtube/v3/videos?part=snippet&key='+YTKey+'&id='+videoId)
+		    .success(function(response){
+			asyncTask.resolve(response);
+		    })
+		    .error(function(reason){
+			_getChannelId[videoId] = undefined;
+			asyncTask.reject(reason);
+		    });
+	    asyncTask.promise.success=function(cb){
+		asyncTask.promise.then(cb,function(){});
+		return asyncTask.promise;
+	    };
+	    asyncTask.promise.error=function(cb){
+		asyncTask.promise.then(function(){},cb);
+		return asyncTask.promise;
+	    };
+	    asyncTask.promise.finally=function(cb){
+		asyncTask.promise.then(cb,cb);
+		return asyncTask.promise;
+	    };
+	    
+	    _getChannelId[videoId] = asyncTask.promise;
+	    return _getChannelId[videoId];
 	};
     }]);
