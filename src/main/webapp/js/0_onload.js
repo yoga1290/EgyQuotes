f3ecf3f6ebd9b0bded7b166d8da1fe27165ef26e
Youssef Gamil <yoga1290@gmail.com>
2017-01-07 12:44:33 +0200

    #13 regex fix

--- src/main/webapp/js/0_onload.js
+++ src/main/webapp/js/0_onload.js
@@ -3,7 +3,7 @@ if (check) {
     window.history.replaceState(null, null, '/#/');
 }
 
-check = window.location.href.match(/access_token=\/.*/) !== null;
+check = window.location.href.match(/access_token=.+/) !== null;
 if (check) {
     window.localStorage.setItem('access_token', window.location.search.replace("?access_token=", ""));
 }
