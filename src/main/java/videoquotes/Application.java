8dfc8dc943eab71fdb3209a21c9d8fd06ef2ebfc
Youssef Gamil <yoga1290@gmail.com>
2017-01-11 20:31:52 +0200

    #16 CORS Origins

--- src/main/java/videoquotes/Application.java
+++ src/main/java/videoquotes/Application.java
@@ -1,10 +1,15 @@
 package videoquotes;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 
 @SpringBootApplication
@@ -14,6 +19,20 @@ public class Application {
     public static void main(String[] args) {	
 	
         ApplicationContext ctx = SpringApplication.run(Application.class, args);
+    }
+
+    // https://spring.io/guides/gs/rest-service-cors/
+    @Value("${videoquotes.origins}")
+    String ORIGINS;
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins(ORIGINS.split(","));
+            }
+        };
     }
 
 }
