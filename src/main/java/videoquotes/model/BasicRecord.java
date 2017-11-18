d3893420829e93b5fd201c7c50e6034c5ebeb779
Youssef Gamil <yoga1290@gmail.com>
2017-11-18 14:18:16 +0200

    Bean Validation #21 : Validator function passed to records

--- src/main/java/videoquotes/model/BasicRecord.java
+++ src/main/java/videoquotes/model/BasicRecord.java
@@ -1,16 +1,37 @@
 package videoquotes.model;
 
 import java.util.Date;
+import java.util.Set;
+
 import org.springframework.data.annotation.Id;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.security.core.context.SecurityContextHolder;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
 
 /**
  *
  * @author yoga1290
  */
 public class BasicRecord {
+
+	public interface PUT {}
+	public interface POST {}
+	public interface DELETE {}
+	// http://beanvalidation.org/latest-draft/spec/#validationapi-constraintviolation-examples
+	public void validate(Class... group) {
+		Set<ConstraintViolation<BasicRecord>> errors = Validation.buildDefaultValidatorFactory().getValidator().validate(this, group);
+		String message = "";
+		for(ConstraintViolation<BasicRecord> error : errors) {
+			message += error.getPropertyPath().toString() + " " + error.getMessage() + ", ";
+		}
+		if( errors.size() > 0) {
+			throw new RuntimeException(message);
+		}
+	}
+
     @Id
     private @Getter @Setter String id;
     private @Getter @Setter Date creationTime;
@@ -19,7 +40,7 @@ public class BasicRecord {
     
     public BasicRecord() {
 	this.creationTime = new Date();
-	
+
 	try {
 	    if (SecurityContextHolder.getContext().getAuthentication() != null) {
 		this.creatorId = SecurityContextHolder.getContext().getAuthentication().getName();
